3afef8ad7fe1360e5d5e1207551d3917e4de8edd
MineDev <168221516+CaveGameDev@users.noreply.github.com>
2025-06-28 14:26:29 -0700

    tweakclass broke

--- src/lib/GameDisplay.svelte
+++ src/lib/GameDisplay.svelte
@@ -43,7 +43,7 @@
 		await cheerpjRunMain(
 			"net.minecraft.launchwrapper.Launch",
 			pathJarLibs,
-			"--tweakClass",      
+			"--tweakClass", "net.minecraft.launchwrapper.AlphaVanillaTweaker"        
 		);
 	}
 
