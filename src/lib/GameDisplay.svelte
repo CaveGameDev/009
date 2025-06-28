d8bb0c59e0d0d5fec2c9ebd3e76f73561d07924e
MineDev <168221516+CaveGameDev@users.noreply.github.com>
2025-06-27 23:35:36 -0700

    Update GameDisplay.svelte for 1.8.9

--- src/lib/GameDisplay.svelte
+++ src/lib/GameDisplay.svelte
@@ -5,7 +5,7 @@
 	// --- Minecraft Launch Configuration ---
 
 	// Main Minecraft c0.0.19a client JAR
-	const pathJarMinecraft = "/app/1.4.7.jar"; 
+	const pathJarMinecraft = "/app/1.8.9.jar"; 
 
 	// LaunchWrapper main JAR
 	const pathJarLaunchWrapper = "/app/launchwrapper-1.6.jar";
