7bb4539ca299b320463950c20e51a82bce37e752
MineDev <168221516+CaveGameDev@users.noreply.github.com>
2025-06-30 22:21:36 -0700

    3/3

--- src/lib/GameDisplay.svelte
+++ src/lib/GameDisplay.svelte
@@ -5,7 +5,7 @@
 	// --- Minecraft Launch Configuration ---
 
 	// Main Minecraft client JAR
-	const pathJarMinecraft = "/app/b1.0.2.jar"; 
+	const pathJarMinecraft = "/app/b1.7.3.jar"; 
 
 	// LaunchWrapper main JAR
 	const pathJarLaunchWrapper = "/app/launchwrapper-1.6.jar";
