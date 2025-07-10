4f2994045a047ea46691471b8a8c9bdb04f41815
MineDev <168221516+CaveGameDev@users.noreply.github.com>
2025-07-10 00:00:50 -0700

    will 3.3.3 fix this?

--- src/lib/GameDisplay.svelte
+++ src/lib/GameDisplay.svelte
@@ -13,7 +13,7 @@
 	// Combine all necessary JARs into the classpath for LaunchWrapper
 	// LWJGL, ASM, and JOpt Simple JARs are now included directly in the classpath string.
 	// Removed Log4j API and Core JARs to diagnose UnsatisfiedLinkError.
-	const pathJarLibs = `/app/lwjgl-assimp-natives-linux.jar:/app/lwjgl/lwjgl-2.9.3.jar:/app/lwjgl/lwjgl_util-2.9.3.jar:${pathJarMinecraft}`;
+	const pathJarLibs = `/app/lwjgl-assimp-natives-linux.jar:/app/lwjgl-natives-linux.jar:/app/lwjgl.jar:/app/lwjgl-javadoc.jar:lwjgl-sources.jar:${pathJarMinecraft}`;
 
 	let display: HTMLDivElement;
 	let intro: HTMLDivElement;
