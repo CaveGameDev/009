df3e97932f3efc0159eda673aaf1bb33d08391b5
MineDev <168221516+CaveGameDev@users.noreply.github.com>
2025-07-09 21:04:30 -0700

    upload

--- src/lib/GameDisplay.svelte
+++ src/lib/GameDisplay.svelte
@@ -44,7 +44,7 @@
 			"net.minecraft.launchwrapper.Launch",
 			pathJarLibs,
 			"--tweakClass", "net.minecraft.launchwrapper.VanillaTweaker",   
-			"--gamedir /app/"
+			"--gamedir /app/",
 			"--username", "WebPlayer"        
 		);
 	}
