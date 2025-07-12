fd6e9401088de9982c7b861fc028fd61c795077b
MineDev <168221516+CaveGameDev@users.noreply.github.com>
2025-07-12 01:20:24 -0700

    Major fix

--- src/lib/GameDisplay.svelte
+++ src/lib/GameDisplay.svelte
@@ -5,7 +5,7 @@
 	// --- Minecraft Launch Configuration ---
 
 	// Main Minecraft client JAR
-	const pathJarMinecraft = "/app/st.jar"; 
+	const pathJarMinecraft = "/app/stFix.jar"; 
 
 	// LaunchWrapper main JAR
 	const pathJarLaunchWrapper = "/app/launchwrapper-1.6.jar";
