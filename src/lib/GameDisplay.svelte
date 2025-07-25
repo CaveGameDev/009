cafe8970835351bf28655d21a5cb8311da64fc4b
SquareTile-Games <168221516+CaveGameDev@users.noreply.github.com>
2025-07-24 23:07:20 -0700

    INFDEV UPGRADE

--- src/lib/GameDisplay.svelte
+++ src/lib/GameDisplay.svelte
@@ -6,7 +6,7 @@
 
 	// Main Minecraft client JAR - This path is within CheerpJ's virtual file system.
 	// It is assumed this JAR is already available at this path.
-	const pathJarMinecraft = "/app/in-20091219-dev.jar";
+	const pathJarMinecraft = "/app/First InfDev.jar";
 
 	// LaunchWrapper main JAR
 	const pathJarLaunchWrapper = "/app/launchwrapper-1.6.jar";
