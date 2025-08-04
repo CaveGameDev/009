be71ffba53f3b3fb5588573d910483b1c66e41d9
SquareTile-Games <168221516+CaveGameDev@users.noreply.github.com>
2025-08-04 02:31:27 -0700

    URL interceptor nu patch

--- src/lib/GameDisplay.svelte
+++ src/lib/GameDisplay.svelte
@@ -5,7 +5,7 @@
 	// --- Minecraft Launch Configuration ---
 	const pathJarMinecraft = "/app/First InfDev.jar";
 	const pathJarLaunchWrapper = "/app/launchwrapper-1.6.jar";
-	const pathJarLibs = `${pathJarLaunchWrapper}:/app/asm-all-4.1.jar:/app/jopt-simple-4.5.jar:/app/lwjgl/lwjgl-2.9.3.jar:/app/lwjgl/lwjgl_util-2.9.3.jar:${pathJarMinecraft}`;
+	const pathJarLibs = `/app/InterceptingJar.jar:${pathJarLaunchWrapper}:/app/asm-all-4.1.jar:/app/jopt-simple-4.5.jar:/app/lwjgl/lwjgl-2.9.3.jar:/app/lwjgl/lwjgl_util-2.9.3.jar:${pathJarMinecraft}`;
 
 	// DOM element references
 	let display: HTMLDivElement;
