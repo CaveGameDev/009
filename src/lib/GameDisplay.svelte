d356198c6a839e925455cfdf27296c51b684f4ef
SquareTile-Games <168221516+CaveGameDev@users.noreply.github.com>
2025-08-06 01:24:31 -0700

    prep

--- src/lib/GameDisplay.svelte
+++ src/lib/GameDisplay.svelte
@@ -5,7 +5,7 @@
 	// --- Minecraft Launch Configuration ---
 	const pathJarMinecraft = "/app/obf.jar";
 	const pathJarLaunchWrapper = "/app/launchwrapper-1.6.jar";
-	const pathJarLibs = `/app/InterceptingJar.jar:${pathJarLaunchWrapper}:/app/asm-all-4.1.jar:/app/jopt-simple-4.5.jar:/app/lwjgl/LWJGL-Jinput.jar:/app/lwjgl/lwjgl_util-2.9.3.jar:${pathJarMinecraft}`;
+	const pathJarLibs = `/app/INF_PLUS_IntJar.jar:${pathJarLaunchWrapper}:/app/asm-all-4.1.jar:/app/jopt-simple-4.5.jar:/app/lwjgl/LWJGL-Jinput.jar:/app/lwjgl/lwjgl_util-2.9.3.jar:${pathJarMinecraft}`;
 
 	// DOM element references
 	let display: HTMLDivElement;
